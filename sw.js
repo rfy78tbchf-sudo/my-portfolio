ee0d9eade2eed7da52af3626879538e818876011
상 <rfy78tbchf@privaterelay.appleid.com>
2026-09-23 18:30:16 +0900

    Redesign mobile portfolio interface and simplify navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='my-portfolio-shell-20260923-23';
+const CACHE='my-portfolio-shell-20260923-24';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.png','./passkey.js'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
