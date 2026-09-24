8847b0e6ceadfae465eb949ce298d8d87a181950
상 <rfy78tbchf@privaterelay.appleid.com>
2026-09-24 17:58:23 +0900

    Refresh iPhone home-screen app shell for trade target release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='my-portfolio-shell-20260924-31';
+const CACHE='my-portfolio-shell-20260924-32';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.png','./passkey.js'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
